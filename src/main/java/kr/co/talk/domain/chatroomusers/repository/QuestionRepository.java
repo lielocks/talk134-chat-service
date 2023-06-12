a67b6929257252369c381f88f9e3bffa4aeb9449
lielocks <kari300084@gmail.com>
2023-06-12 14:31:27 +0900

    [refactor]

--- src/main/java/kr/co/talk/domain/chatroomusers/repository/QuestionRepository.java
+++ src/main/java/kr/co/talk/domain/chatroomusers/repository/QuestionRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    Question findByQuestionIdAndKeyword_KeywordId(Long questionId, Long keywordId);
     List<Question> findByKeyword_KeywordId(Long keywordId);
 }

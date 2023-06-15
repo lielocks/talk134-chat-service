fe81534abe74d922fc39928a26497fd054903958
shmin7777 <shmin7777@naver.com>
2023-06-16 01:17:22 +0900

    feedback create api 완료

--- src/main/java/kr/co/talk/domain/chatroom/controller/ChatroomController.java
+++ src/main/java/kr/co/talk/domain/chatroom/controller/ChatroomController.java
@@ -1,6 +1,8 @@
 package kr.co.talk.domain.chatroom.controller;
 
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,8 @@ import kr.co.talk.domain.chatroom.dto.RequestDto.CreateChatroomResponseDto;
 import kr.co.talk.domain.chatroom.dto.RequestDto.UserIdResponseDto;
 import kr.co.talk.domain.chatroom.service.ChatRoomService;
 import kr.co.talk.global.client.UserClient;
+import kr.co.talk.global.constants.RedisConstants;
+import kr.co.talk.global.service.redis.RedisService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,83 +27,83 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/chat")
 public class ChatroomController {
 
-    private final UserClient userClient;
-    private final ChatRoomService chatRoomService;
+	private final UserClient userClient;
+	private final ChatRoomService chatRoomService;
+	private final RedisService redisService;
 
-    /**
-     * 대화방 목록 조회 api
-     * 
-     * @param userId
-     * @param teamCode
-     * @return
-     */
-    @GetMapping("/find-chatrooms")
-    public ResponseEntity<?> findChatRooms(@RequestHeader(value = "userId") Long userId) {
-        String teamCode = userClient.getTeamCode(userId).getTeamCode();
-        return ResponseEntity.ok(chatRoomService.findChatRooms(userId, teamCode));
-    }
+	/**
+	 * 대화방 목록 조회 api
+	 * 
+	 * @param userId
+	 * @param teamCode
+	 * @return
+	 */
+	@GetMapping("/find-chatrooms")
+	public ResponseEntity<?> findChatRooms(@RequestHeader(value = "userId") Long userId) {
+		return ResponseEntity.ok(chatRoomService.findChatRooms(userId));
+	}
 
-    /**
-     * 닉네임 또는 이름으로 대화방 목록 조회 api
-     * 
-     * @param userId
-     * @param teamCode
-     * @param name
-     * @return
-     */
-    @GetMapping("/find-chatrooms-with-name")
-    public ResponseEntity<?> findChatRoomsWithName(@RequestHeader(value = "userId") Long userId,
-            String name) {
-        String teamCode = userClient.getTeamCode(userId).getTeamCode();
-        List<UserIdResponseDto> userIdResponseDtos = userClient.getUserIdByName(teamCode, name);
-        return ResponseEntity
-                .ok(chatRoomService.findChatRoomsByName(userId, teamCode, userIdResponseDtos));
-    }
+	/**
+	 * 닉네임 또는 이름으로 대화방 목록 조회 api
+	 * 
+	 * @param userId
+	 * @param teamCode
+	 * @param name
+	 * @return
+	 */
+	@GetMapping("/find-chatrooms-with-name")
+	public ResponseEntity<?> findChatRoomsWithName(@RequestHeader(value = "userId") Long userId, String name) {
+		return ResponseEntity.ok(chatRoomService.findChatRoomsByName(userId, name));
+	}
 
-    /**
-     * 대화방 생성 api
-     * 
-     * @param teamCode
-     * @param userList
-     * @return
-     */
-    @PostMapping("/create-chatroom")
-    public ResponseEntity<?> createChatroom(@RequestHeader(value = "userId") Long userId,
-            @RequestBody List<Long> userList) {
-        chatRoomService.createChatroom(userId, userList);
-        return ResponseEntity.ok().build();
-    }
+	/**
+	 * 대화방 생성 api
+	 * 
+	 * @param teamCode
+	 * @param userList
+	 * @return
+	 */
+	@PostMapping("/create-chatroom")
+	public ResponseEntity<?> createChatroom(@RequestHeader(value = "userId") Long userId,
+			@RequestBody List<Long> userList) {
+		chatRoomService.createChatroom(userId, userList);
+		return ResponseEntity.ok().build();
+	}
 
-    /**
-     * 피드백 선택형 등록 api
-     */
-    @PostMapping("/create/feedback/optional")
-    public ResponseEntity<?> feedbackCreateOptional(
-            @RequestBody FeedbackDto feedbackDto) {
-        log.info("feedbackOptionalDto::::" + feedbackDto);
-        chatRoomService.saveFeedbackOptionalToRedis(feedbackDto);
-        return ResponseEntity.ok().build();
-    }
+	/**
+	 * 피드백 선택형 등록 api
+	 */
+	@PostMapping("/create/feedback/optional")
+	public ResponseEntity<?> feedbackCreateOptional(@RequestBody FeedbackDto feedbackDto) {
+		log.info("feedbackOptionalDto::::" + feedbackDto);
+		chatRoomService.saveFeedbackOptionalToRedis(feedbackDto);
+		return ResponseEntity.ok().build();
+	}
 
-    /**
-     * 피드백 필수형 등록 api
-     */
-    @PostMapping("/create/feedback")
-    public ResponseEntity<?> feedbackCreateOptional(@RequestHeader(value = "userId") Long userId,
-            @RequestBody FeedbackDto feedbackDto) {
-        log.info("feedbackOptionalDto::::" + feedbackDto);
-        chatRoomService.saveFeedbackToRedis(userId, feedbackDto);
-        return ResponseEntity.ok().build();
-    }
+	/**
+	 * 피드백 필수형 등록 api
+	 */
+	@PostMapping("/create/feedback")
+	public ResponseEntity<?> feedbackCreateOptional(@RequestHeader(value = "userId") Long userId,
+			@RequestBody FeedbackDto feedbackDto) {
+		log.info("feedbackOptionalDto::::" + feedbackDto);
 
-    /**
-     * 피드백 필수형 조회
-     * @param userId
-     * @return
-     */
-    @GetMapping("/find/feedback")
-    public ResponseEntity<?> findFeedback(@RequestHeader(value = "userId") Long userId) {
-        return ResponseEntity.ok(userClient.getUserStaus(userId));
-    }
+		Map<String, Object> entry = redisService.getEntry(RedisConstants.FEEDBACK_ + feedbackDto.getRoomId(),
+				FeedbackDto.class);
+
+		chatRoomService.saveFeedbackToRedis(userId, feedbackDto, entry);
+		return ResponseEntity.ok().build();
+	}
+
+	/**
+	 * 피드백 필수형 조회
+	 * 
+	 * @param userId
+	 * @return
+	 */
+	@GetMapping("/find/feedback")
+	public ResponseEntity<?> findFeedback(@RequestHeader(value = "userId") Long userId) {
+		return ResponseEntity.ok(userClient.getUserStaus(userId));
+	}
 
 }
--- src/main/java/kr/co/talk/domain/chatroom/dto/FeedbackDto.java
+++ src/main/java/kr/co/talk/domain/chatroom/dto/FeedbackDto.java
@@ -1,5 +1,6 @@
 package kr.co.talk.domain.chatroom.dto;
 
+import java.io.Serializable;
 import java.util.List;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
@@ -13,31 +14,35 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class FeedbackDto {
-    private long userId;
-    private long roomId;
-    private String sentence;
-    private int score;
-    
-    private int statusEnergy;
-    private int statusRelation;
-    private int statusStress;
-    private int statusStable;
-    
-    private int statusEnergyBefore;
-    private int statusRelationBefore;
-    private int statusStressBefore;
-    private int statusStableBefore;
-    
-    private List<Feedback> feedback;
-    
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor(access = AccessLevel.PROTECTED)
-    private static class Feedback {
-        private long toUserId;
-        private String review;
-        private int feedbackScore;
-    }
-    
+public class FeedbackDto implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private long userId;
+	private long roomId;
+	private String sentence;
+	private int score;
+
+	private int statusEnergy;
+	private int statusRelation;
+	private int statusStress;
+	private int statusStable;
+
+	private int statusEnergyBefore;
+	private int statusRelationBefore;
+	private int statusStressBefore;
+	private int statusStableBefore;
+
+	private List<Feedback> feedback;
+
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor(access = AccessLevel.PROTECTED)
+	private static class Feedback implements Serializable {
+		private static final long serialVersionUID = 1L;
+		
+		private long toUserId;
+		private String review;
+		private int feedbackScore;
+	}
+
 }
